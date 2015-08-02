3c5c1abc8f5e00e5a0e86416ad65e48bffcaf0b1
Chuck Bolin <cbolin@sc.rr.com>
2015-08-01 20:49:56 -0400

    Main activity contains four fragments.

--- app/src/main/java/com/mush4brain/flashreporting/MainActivity.java
+++ app/src/main/java/com/mush4brain/flashreporting/MainActivity.java
@@ -1,36 +1,108 @@
 package com.mush4brain.flashreporting;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends Activity {
+  private String TAG = "MainActivity";
+  FragmentTransaction ft = null;
+  FragTop fragTop = new FragTop();
+  FragBottom fragBottom = new FragBottom();
+  FragLeft fragLeft = new FragLeft();
+  FragRight fragRight = new FragRight();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+    ft = getFragmentManager().beginTransaction();
+    ft.add(R.id.container_top, fragTop);
+    ft.add(R.id.container_left,  fragLeft);
+    ft.add(R.id.container_right, fragRight);
+    ft.add(R.id.container_bottom, fragBottom);
+    ft.commit();
+  }
+
+  //top fragment
+  public static class FragTop extends Fragment implements View.OnClickListener {
+    public FragTop() {
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View rootView = inflater
+              .inflate(R.layout.container_top, container, false);
+      return rootView;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v){
     }
+  }
+
+  //bottom fragment
+  public static class FragBottom extends Fragment implements View.OnClickListener {
+    public FragBottom() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View rootView = inflater
+              .inflate(R.layout.container_bottom, container, false);
+      return rootView;
+    }
+
+    @Override
+    public void onClick(View v){
+    }
+  }
+
+  //left fragment
+  public static class FragLeft extends Fragment implements View.OnClickListener {
+    public FragLeft() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View rootView = inflater
+              .inflate(R.layout.container_left, container, false);
+      return rootView;
+    }
+
+    @Override
+    public void onClick(View v){
+    }
+  }
+
+  //right fragment
+  public static class FragRight extends Fragment implements View.OnClickListener {
+    public FragRight() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View rootView = inflater
+              .inflate(R.layout.container_right, container, false);
+      return rootView;
+    }
+
+    @Override
+    public void onClick(View v){
+    }
+  }
 }
